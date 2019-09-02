4e9e7adf0ffaff1b1982ce75d0d7eff9616eaef5
Yuliya Meleshko <yuliya.meleshko@itechart-group.com>
2019-09-02 16:49:26 +0300

    Fixed code

--- src/components/Orders/Order.js
+++ src/components/Orders/Order.js
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { getToken } from "./../../helpers/authHelper";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { getToken } from './../../helpers/authHelper';
 
 const Order = () => {
-  const api = axios.create({
-    baseURL: "/"
-  });
-  const [orders, setOrders] = useState(null);
-  useEffect(
-    (orders, api) => {
-      const fetchData = async (api, orders) => {
-        const data = await api.get("/flight", {
-          headers: {
-            Authorisation: getToken()
-          }
-        });
-        setOrders(data.data);
-      };
-      fetchData(orders, api);
-      console.log(orders);
-    },
-    [orders]
-  );
-  return <h1>Hello</h1>;
+	const [orders, setOrders] = useState(null);
+	useEffect(() => {
+		if (!orders) {
+			const fetchData = async () => {
+				const api = axios.create({
+					baseURL: '/'
+				});
+				const data = await api.get('/flight', {
+					headers: {
+						Authorisation: getToken()
+					}
+				});
+				setOrders(data.data);
+			};
+			fetchData();
+		}
+	});
+
+	console.log(orders);
+	return <h1>Hello</h1>;
 };
 export default Order;
